63cee074375087b9c4e7de3b6c6a8f669ce8a0a2
Иван Шульженко <ilitetop@yandex.ru>
2022-04-07 00:07:23 +0700

    интерфейсы

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -11,7 +11,7 @@ export function getPerson(): IPersonData {
     return {
         name: 'Petr',
         secondName: 'Smith',
-        age: '7',
+        age: 7,
     };
 }
 
@@ -27,6 +27,28 @@ export interface IStudentData {
 Данные должны храниться в объектах, соответствующих интерфейсу `IStudentData`.
  */
 export function getStudentsData(): IStudentData[] {
+    let result: IStudentData[] = [
+        {
+            name: 'Ivan',
+            secondName: 'Petrov',
+            age: 20,
+            phone: '+7(555)555-55-50',
+        },
+        {
+            name: 'Stepan',
+            secondName: 'Petrov',
+            age: 19,
+            phone: '+7(555)555-55-51',
+        },
+        {
+            name: 'Petr',
+            secondName: 'Ivanov',
+            age: 19,
+        },
+    ];
+
+    return result;
+
     /**
      * Данные студентов которые нужно передать:
      *
